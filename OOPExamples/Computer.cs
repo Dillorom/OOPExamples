987904ead8c05fafd184f5c9447847788a36b5cc
Dillorom <37942024+Dillorom@users.noreply.github.com>
2019-01-18 21:39:53 -0800

    Add project files.

---
+++ OOPExamples/Computer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPExamples
+{
+    class Computer
+    {
+        protected string _name;
+        public string name
+        {
+            get
+            {
+                return _name;
+            }
+            private set
+            {
+                _name = value;
+            }
+        }
+        public bool isOn { get; private set; }
+
+        public Computer(string name)
+        {
+            this.name = name;
+        }
+
+        protected void TurnOn()
+        {
+            isOn = true;
+        }
+
+        protected void TurnOff()
+        {
+            isOn = false;
+        }
+
+        public void TogglePower()
+        {
+            if(isOn)
+            {
+                TurnOff();
+            }
+            else
+            {
+                TurnOn();
+            }
+        }
+    }
+}
---
+++ OOPExamples/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OOPExamples;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var comp = new Computer("Dell");
+        
+
+        var isComputer = comp is Computer;
+
+        Console.WriteLine("Computer's name is " + comp.name);
+        Console.WriteLine("Is computer " + isComputer);
+        comp.TogglePower();
+        Console.WriteLine("The computer is on - " + comp.isOn);
+        
+        Console.ReadKey();
+    }
+}
+   
+
